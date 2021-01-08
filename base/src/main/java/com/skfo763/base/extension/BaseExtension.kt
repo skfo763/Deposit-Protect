0d69f618ca6c85eaf0358d6ae278058188ca4b16
Changyeon-Seo <scy7351@naver.com>
2021-01-08 20:40:06 +0900

    기본 Firebase 크래시리틱스 로그 추가 함수 작성

--- base/src/main/java/com/skfo763/base/extension/BaseExtension.kt
+++ base/src/main/java/com/skfo763/base/extension/BaseExtension.kt
@@ -1,4 +1,37 @@
 package com.skfo763.base.extension
 
-class BaseExtension {
+import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.skfo763.base.BuildConfig
+
+enum class LogType {
+    CRITICAL,
+    NON_CRITICAL
+}
+
+fun Any.logMessage(message: String?) {
+    if(BuildConfig.DEBUG) {
+        Log.d(this::class.java.simpleName, message ?: "unkwnown error")
+    }
+}
+
+fun Any.logError(message: String?) {
+    if(BuildConfig.DEBUG) {
+        Log.e(this::class.java.simpleName, message ?: "unknown error")
+    } else {
+        FirebaseCrashlytics.getInstance().log(message ?: "unknown error")
+    }
+}
+
+fun Any.logException(e: Exception, logType: LogType = LogType.CRITICAL) {
+    when(logType) {
+        LogType.CRITICAL -> if(BuildConfig.DEBUG) {
+            e.printStackTrace()
+        } else {
+            FirebaseCrashlytics.getInstance().recordException(e)
+        }
+        LogType.NON_CRITICAL -> {
+            Log.e(this::class.java.simpleName, "Exception has occured", e)
+        }
+    }
 }
